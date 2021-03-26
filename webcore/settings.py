90e93f1e6d7384b1b4e2bbbb37397cb2bf37113b
Gabriel Erwhian Winarso <gabrielerwhian@gmail.com>
2021-03-26 18:45:29 +0700

    updated settings.py

--- webcore/settings.py
+++ webcore/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'f9-hr(lrsky8elxzyraz5yn%xbac-&wgjva7(3rfuxiee5@bh&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
